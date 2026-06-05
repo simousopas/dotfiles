22613e3a75a69fcaa03919c7ad2d07e4bea118e3
Fernando <fernando@simousopas.com>
2026-06-05 21:53:25 +0100

    Add services to enable-maintenance-services.sh

--- etc/macos/scripts/enable-maintenance-services.sh
+++ etc/macos/scripts/enable-maintenance-services.sh
@@ -4,11 +4,15 @@ echo "Enabling user services ..."
 uid=$(id -u)
 launchctl enable "gui/$uid/com.apple.appstoreagent"
 launchctl enable "gui/$uid/com.apple.appstorecomponentsd"
+launchctl enable "gui/$uid/com.apple.SoftwareUpdateNotificationManager"
 
 echo "Enabling system services ..."
 macos_major_version="$(sw_vers -productVersion | grep -o '^\d*')"
 if [ $((macos_major_version)) -ne 26 ]; then
 	sudo launchctl enable "system/com.apple.security.syspolicy"
 fi
+sudo launchctl enable "system/com.apple.appstored"
+sudo launchctl enable "system/com.apple.AppStoreDaemon.StorePrivilegedODRService"
+sudo launchctl enable "system/com.apple.AppStoreDaemon.StorePrivilegedTaskService"
 sudo launchctl enable "system/com.apple.mobile.softwareupdated"
 sudo launchctl enable "system/com.apple.softwareupdated"
