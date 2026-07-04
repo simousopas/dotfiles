19f36a8a066f556efd7620c9bf806488294edd85
Fernando <fernando@simousopas.com>
2026-07-04 20:33:01 +0100

    Add `install-batt.sh`
    - This script will install charlie0129/batt.
    - Added it to the bootstrap script of macbookpro-m2max.

--- macos/macbookpro-m2max/bootstrap.sh
+++ macos/macbookpro-m2max/bootstrap.sh
@@ -112,6 +112,10 @@ MISE_YES=1 mise install
 # fish --command "bun completions"
 
 
+log_info "\t >>> Installing batt ..."
+/bin/bash "$root_dir/etc/macos/scripts/install-batt.sh"
+
+
 log_info "\t >>> Installing iSMC ..."
 /bin/bash "$root_dir/etc/macos/scripts/install-ismc.sh"
 ismc completion bash >"$HOME/.bash_completion.d/iscm.sh"
