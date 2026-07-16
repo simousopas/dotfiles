4d2246cb2d7ac26af04607b770949c483e8e7b0b
Fernando <fernando@computoma.com>
2026-07-16 08:16:40 +0100

    Improve toggle-application-lock.sh
    Add the possibility to only toggle a single app by using the
    `--app-name` switch.

--- etc/macos/scripts/toggle-application-lock.sh
+++ etc/macos/scripts/toggle-application-lock.sh
@@ -6,10 +6,16 @@
 # This script automatically toggles read-only permissions for a set of apps
 # to prevent them being updated automatically by their own update systems.
 # Consecutive runs of this script will toggle those permissions back and forth.
+#
+# The following switches are available:
+# -v: Print major commands being executed.
+# -vv: Print major commands being executed and their output.
+# --app-name: Only toggle this specific app.
 
 set -Eeuo pipefail
 
 readonly TMP_LOG_FILE="$TMPDIR/${0##*/}.log"
+app_name=""
 verbose=0
 
 log () { printf '[%s] %s\n' "${0##*/}" "$*" >&2; }
@@ -55,6 +61,9 @@ parse_input_args () {
 		-vv)
 			verbose=2
 			shift;;
+		--app-name)
+			app_name="$2"
+			shift; shift;;
 		*)
 			shift;;
 	esac; done
@@ -76,9 +85,11 @@ toggle_app_lock () {
 		"Spotify"
 		"Visual Studio Code"
 		"WhatsApp"
+		"Xenia-edge"
 		"Zed"
 		"Zoom"
 	)
+	[[ -n $app_name ]] && apps_list=("$app_name")
 
 	for app in "${apps_list[@]}"; do
 		local app_path="/Applications/${app}.app"
